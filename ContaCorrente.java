df405a025dcea84f624bc7ca43e02d44f1414081
Sergio Rubens de Campos Cruz <sergio_ru@hotmail.com>
2022-12-03 10:12:00 -0300

    03-12-2022

--- ContaCorrente.java
+++ ContaCorrente.java
@@ -3,4 +3,28 @@ public class ContaCorrente {
     private int agencia;
     private int numero;
     private double saldo;
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void sacar() {
+
+    }
+
+    public void deposittar() {
+
+    }
+
+    public void transferir() {
+
+    }
 }
--- ContaPoupanca.java
+++ ContaPoupanca.java
@@ -2,5 +2,28 @@ public class ContaPoupanca {
     private int agencia;
     private int numero;
     private double saldo;
-    
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void sacar() {
+
+    }
+
+    public void deposittar() {
+
+    }
+
+    public void transferir() {
+
+    }
 }
